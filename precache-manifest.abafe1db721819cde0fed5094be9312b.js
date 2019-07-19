636af1a9a3a89120802bb283f4040ce70de49d96
Thiago Spindola <thiago.spindola@aapvr.com.br>
2019-07-19 13:46:24 -0300

    Updates

---
+++ precache-manifest.abafe1db721819cde0fed5094be9312b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa75fbcbd87a222b14a910cea5ed23e8",
+    "url": "/ame-refactory/index.html"
+  },
+  {
+    "revision": "69718e1e877c5acfa484",
+    "url": "/ame-refactory/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "70c479c6c2d382cde8ea",
+    "url": "/ame-refactory/static/css/main.1474b932.chunk.css"
+  },
+  {
+    "revision": "69718e1e877c5acfa484",
+    "url": "/ame-refactory/static/js/2.1ae99150.chunk.js"
+  },
+  {
+    "revision": "70c479c6c2d382cde8ea",
+    "url": "/ame-refactory/static/js/main.b5ef81a8.chunk.js"
+  },
+  {
+    "revision": "6589ccf74b57d24c025c",
+    "url": "/ame-refactory/static/js/runtime~main.c5084991.js"
+  },
+  {
+    "revision": "972d54ac7985dbc9a8014a49a29c7668",
+    "url": "/ame-refactory/static/media/ShareTechMono-Regular.972d54ac.ttf"
+  }
+]);
